5a4f5090bfbd807b52211b503d7f7a311ae97f8d
Marj Etienne <marj.etienne@trupanion.com>
2021-03-10 15:53:03 -0800

    added properties and methods to define the BankAccount class

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,6 +4,18 @@ namespace Classes
 {
     public class BankAccount
     {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
 
+        public void MakeDepositee(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
     }
 }
