5d6778081d5dd73a161f0f6358910422060c456d
Marj Etienne <marj.etienne@trupanion.com>
2021-03-10 17:48:56 -0800

    added inline testing with try and catch statements

--- Program.cs
+++ Program.cs
@@ -15,6 +15,28 @@ namespace Classes
             account.MakeDeposite(100, DateTime.Now, "Friend paid me back");
             Console.WriteLine(account.Balance);
 
+            // Inline Testing
+            // Test that the initial balance must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
